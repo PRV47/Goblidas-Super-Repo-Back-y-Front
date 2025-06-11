d4aee61d63b501de5216219ef953f4f11189305a
PRV47 <piersrideout@gmail.com>
2025-06-11 15:40:39 -0300

    Arreglado la ventana Mis Pedidos para el usuario

--- goblidas-backend/src/main/java/com/example/goblidas_backend/repositories/OrderRepository.java
+++ goblidas-backend/src/main/java/com/example/goblidas_backend/repositories/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.example.goblidas_backend.repositories;
 
 import com.example.goblidas_backend.entities.Order;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,5 +10,6 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends BaseRepository<Order, Long> {
 
-    List<Order> findByUserAdressIdUserId_Id(Long userId);
+    @Query("SELECT o FROM Order o WHERE o.userAdressId.userId.id = :userId")
+    List<Order> findByUserAdressIdUserId_Id(@Param("userId") Long userId);
 }
